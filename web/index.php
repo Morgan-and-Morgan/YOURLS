3b0529e3dedddb2672698e4df25b46df9702bf79
Bill Van Pelt <linusx@gmail.com>
2016-09-02 15:15:21 -0400

    Fixing location

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "/../../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 $dotenv = new Dotenv\Dotenv( dirname( __DIR__ . "/../" ) );
 $dotenv->load();
